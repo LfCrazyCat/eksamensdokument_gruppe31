e8a46c0ba46a1da77d9a50a45ba315e31bb733ba
LfCrazyCat <89871713+LfCrazyCat@users.noreply.github.com>
2024-05-20 23:48:36 +0200

    sanity.cli.js oppdatert

---
+++ backend/sanity.cli.js
@@ -0,0 +1,11 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'glvgqvzp',
+    dataset: 'production'
+  },
+  server: {
+    port: 3334 
+  }
+})

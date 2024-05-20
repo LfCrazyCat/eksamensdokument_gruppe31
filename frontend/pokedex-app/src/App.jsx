231d0467c8ef1d8e5a589bdf06a47774d54062db
LfCrazyCat <89871713+LfCrazyCat@users.noreply.github.com>
2024-05-20 22:43:20 +0200

    Oppdatert App.jsx

--- frontend/pokedex-app/src/App.jsx
+++ frontend/pokedex-app/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//pokedex-app/src/pages/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Pokemon from './pages/Pokemon';
+import Type from './pages/Type';
+import SearchResult from './pages/SearchResult';
+import Teams from './pages/Teams';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <header>
+        <nav>
+          <a href="/">UIN POKEDEX</a>
+          <a href="/teams">Teams</a>
+          <input type="text" placeholder="Search..." />
+          <button><img src="/assets/search-icon.png" alt="Search" /></button>
+        </nav>
+      </header>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/teams" component={Teams} />
+        <Route path="/:type" component={Type} />
+        <Route path="/searchresults/:pokemon" component={SearchResult} />
+        <Route path="/pokemons/:pokemon" component={Pokemon} />
+      </Switch>
+    </Router>
+  );
+};
 
-export default App
+export default App;

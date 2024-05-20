c83b243ed12826e5af84566ffa047e16f6a6191a
LfCrazyCat <89871713+LfCrazyCat@users.noreply.github.com>
2024-05-20 23:22:16 +0200

    Oppdatert main.jsx

--- frontend/pokedex-app/src/main.jsx
+++ frontend/pokedex-app/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App';
 import './styles/style.css';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root')); //opprettet root 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>

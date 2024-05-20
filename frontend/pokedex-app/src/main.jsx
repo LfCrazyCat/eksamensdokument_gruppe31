dca743949ccf15df1e2d9714fbca2c3e67868d6e
LfCrazyCat <89871713+LfCrazyCat@users.noreply.github.com>
2024-05-20 22:57:35 +0200

    Endringer i main.jsx

--- frontend/pokedex-app/src/main.jsx
+++ frontend/pokedex-app/src/main.jsx
@@ -1,12 +1,11 @@
 //pokedex-app/src/main.jsx
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/style.css';
 
-
-const container = document.getElementById('root');
-const root = createRoot(container); 
+//Opprettet root
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>

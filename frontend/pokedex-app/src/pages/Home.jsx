d45f82650616d8887e158c10910e9a7971f74ffc
LfCrazyCat <89871713+LfCrazyCat@users.noreply.github.com>
2024-05-21 10:54:07 +0200

    Update i Home.jsx

--- frontend/pokedex-app/src/pages/Home.jsx
+++ frontend/pokedex-app/src/pages/Home.jsx
@@ -1,8 +1,48 @@
-// pokedex-app/src/pages/Home.jsx
+// frontend/pokedex-app/src/pages/Home.jsx
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import '../styles/style.css';
+
+
+import normal from '../assets/type-symbols/Normal.png';
+import fighting from '../assets/type-symbols/Fighting.png';
+import flying from '../assets/type-symbols/Flying.png';
+import poison from '../assets/type-symbols/Poison.png';
+import ground from '../assets/type-symbols/Ground.png';
+import rock from '../assets/type-symbols/Rock.png';
+import bug from '../assets/type-symbols/Bug.png';
+import ghost from '../assets/type-symbols/Ghost.png';
+import steel from '../assets/type-symbols/Steel.png';
+import fire from '../assets/type-symbols/Fire.png';
+import water from '../assets/type-symbols/Water.png';
+import grass from '../assets/type-symbols/Grass.png';
+import electric from '../assets/type-symbols/Electric.png';
+import psychic from '../assets/type-symbols/Psychic.png';
+import ice from '../assets/type-symbols/Ice.png';
+import dragon from '../assets/type-symbols/Dragon.png';
+import dark from '../assets/type-symbols/Dark.png';
+import fairy from '../assets/type-symbols/Fairy.png';
+
+const typeIcons = {
+  normal,
+  fighting,
+  flying,
+  poison,
+  ground,
+  rock,
+  bug,
+  ghost,
+  steel,
+  fire,
+  water,
+  grass,
+  electric,
+  psychic,
+  ice,
+  dragon,
+  dark,
+  fairy,
+};
 
 const Home = () => {
   const [pokemons, setPokemons] = useState([]);
@@ -39,6 +79,7 @@ const Home = () => {
         <div className="types-grid">
           {types.map((type) => (
             <Link key={type.name} to={`/${type.name}`} className={`type-card ${type.name}`}>
+              <img src={typeIcons[type.name.toLowerCase()]} alt={type.name} />
               {type.name}
             </Link>
           ))}
